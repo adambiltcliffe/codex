a8df467c1044edd3fea824d1a35ffce7d70b8d81
Adam Biltcliffe <adam.biltcliffe@gmail.com>
2019-09-13 21:02:07 +0100

    add dist options for node, browser and module

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,50 @@
 module.exports = {
-  presets: ["@babel/preset-env"]
+  presets: ["@babel/preset-env"],
+  env: {
+    development: {
+      ignore: ["**/*.test.js"],
+      presets: [
+        [
+          "@babel/preset-env",
+          {
+            targets: {
+              node: "10.15"
+            }
+          }
+        ]
+      ],
+      plugins: []
+    },
+    browser: {
+      ignore: ["**/*.test.js"],
+      presets: [
+        [
+          "@babel/preset-env",
+          {
+            targets: {
+              browsers: "last 2 versions, ie 11"
+            },
+            modules: false
+          }
+        ]
+      ],
+      plugins: []
+    },
+    module: {
+      ignore: ["**/*.test.js"],
+      presets: [
+        [
+          "@babel/preset-env",
+          {
+            targets: {
+              node: "10.15"
+            },
+            modules: false
+          }
+        ]
+      ],
+      plugins: []
+    }
+  },
+  sourceMaps: true
 };
