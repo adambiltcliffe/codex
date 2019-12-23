8d765dc1d2ebd067d7e8b28cb80eb877ef838955
Adam Biltcliffe <adam.biltcliffe@gmail.com>
2019-12-23 15:14:05 +0000

    added a trigger for dealing damage (not tested yet)

--- src/damage.js
+++ src/damage.js
@@ -2,10 +2,14 @@ import cardInfo from "./cardinfo";
 import log from "./log";
 
 import groupBy from "lodash/groupBy";
+import mapValues from "lodash/mapValues";
 import partition from "lodash/partition";
 import pick from "lodash/pick";
+import sum from "lodash/sum";
 import upperFirst from "lodash/upperFirst";
 import { andJoin } from "./util";
+import { getAbilityDefinition } from "./entities";
+import { createTrigger } from "./triggers";
 
 export function queueDamage(state, damage) {
   if (damage.amount < 1) {
@@ -18,8 +22,46 @@ export function queueDamage(state, damage) {
 }
 
 export function applyPendingDamage(state) {
+  if (state.pendingDamage.length == 0) {
+    return;
+  }
   describeDamageAndQueueTriggers(state);
   state.pendingDamage.forEach(d => describeDamagePacket(state, d));
+  const packetsPerSource = groupBy(state.pendingDamage, "sourceId");
+  const damagePerSubject = mapValues(
+    groupBy(state.pendingDamage, "subjectId"),
+    packets => sum(packets.map(p => p.amount))
+  );
+  const killedEntities = Object.entries(state.entities).filter(([id, e]) => {
+    return (
+      damagePerSubject[id] !== undefined &&
+      damagePerSubject[id] + e.damage > e.current.hp + e.armor
+    );
+  });
+  Object.keys(packetsPerSource).forEach(id => {
+    const e = state.entities[id];
+    if (e !== undefined) {
+      e.current.abilities.forEach(a => {
+        const ad = getAbilityDefinition(a);
+        if (ad.triggerOnDamageEntity) {
+          packetsPerSource[id].forEach(packet => {
+            const isLethal = killedEntities[subjectId] !== undefined;
+            if (
+              !ad.shouldTrigger ||
+              ad.shouldTrigger({ state, packet, isLethal })
+            ) {
+              createTrigger(state, {
+                path: a.path,
+                isLethal,
+                subjectController:
+                  state.entities[packet.subjectId].current.controller
+              });
+            }
+          });
+        }
+      });
+    }
+  });
   state.pendingDamage.forEach(d => applyDamagePacket(state, d));
   state.pendingDamage = [];
 }
